c5b9df7d486ea8a0ee3ab52e0c84509c5391713b
beverlyosoria <bevosoria@gmail.com>
2019-11-19 23:43:55 -0600

    sets up files

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './src/App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header" />
+		</div>
+	);
+}
+
+export default App;
