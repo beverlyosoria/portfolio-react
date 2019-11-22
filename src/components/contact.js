2c3fdbd26f7bf469f580b1414aaf7824aabc6422
beverlyosoria <bevosoria@gmail.com>
2019-11-22 02:44:12 -0600

    adds list on contact page, styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
 	render() {
@@ -7,7 +7,7 @@ class Contact extends Component {
 			<div className="contact-body">
 				<Grid className="contact-grid">
 					<Cell col={6}>
-						Beverly Osoria
+						<h2>Beverly Osoria</h2>
 						<img
 							src="https://c7.uihere.com/files/874/871/627/pop-art-comics-female-comic-book-beauty-avatar-thumb.jpg"
 							alt="avatar"
@@ -19,7 +19,32 @@ class Contact extends Component {
 							commodi ea rerum explicabo ut magni quos?
 						</p>
 					</Cell>
-					<Cell col={6}>Half page</Cell>
+					<Cell col={6}>
+						<h2>Contact Me</h2>
+						<hr />
+						<div className="contact-list">
+							<List>
+								<ListItem>
+									<ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+										<i className="fa fa-phone-square" arria-hidden="true" />
+										Bryan Cranston
+									</ListItemContent>
+								</ListItem>
+								<ListItem>
+									<ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+										<i className="fa fa-phone-square" arria-hidden="true" />
+										Bryan Cranston
+									</ListItemContent>
+								</ListItem>
+								<ListItem>
+									<ListItemContent style={{ fontSize: '25px', fontFamily: 'Anton' }}>
+										<i className="fa fa-phone-square" arria-hidden="true" />
+										Bryan Cranston
+									</ListItemContent>
+								</ListItem>
+							</List>
+						</div>
+					</Cell>
 				</Grid>
 			</div>
 		);
