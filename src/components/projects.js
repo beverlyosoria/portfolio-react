c43c1ec129a186ca23c9a182df5a8dc1f6469014
beverlyosoria <bevosoria@gmail.com>
2019-11-22 02:03:56 -0600

    creates and styles cards

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
 	constructor(props) {
@@ -9,8 +9,59 @@ class Projects extends Component {
 	toggleCategories() {
 		if (this.state.activeTab === 0) {
 			return (
-				<div>
-					<h1>This is React</h1>
+				<div className="projects-grid">
+					{/* Project 1 */}
+					<Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+						<CardTitle
+							style={{
+								color: '#fff',
+								height: '176px',
+								background:
+									'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover'
+							}}
+						>
+							React Project 1
+						</CardTitle>
+						<CardText>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates consequuntur quo odio
+							quia reprehenderit distinctio qui et officiis illum facilis iusto molestiae dignissimos,
+							commodi ea rerum explicabo ut magni quos?
+						</CardText>
+						<CardActions border>
+							<Button colored>GitHub</Button>
+							<Button colored>GitHub</Button>
+							<Button colored>GitHub</Button>
+						</CardActions>
+						<CardMenu style={{ color: '#fff' }}>
+							<IconButton name="share" />
+						</CardMenu>
+					</Card>
+					{/* Project 2 */}
+					<Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+						<CardTitle
+							style={{
+								color: '#fff',
+								height: '176px',
+								background:
+									'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover'
+							}}
+						>
+							React Project 2
+						</CardTitle>
+						<CardText>
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates consequuntur quo odio
+							quia reprehenderit distinctio qui et officiis illum facilis iusto molestiae dignissimos,
+							commodi ea rerum explicabo ut magni quos?
+						</CardText>
+						<CardActions border>
+							<Button colored>GitHub</Button>
+							<Button colored>GitHub</Button>
+							<Button colored>GitHub</Button>
+						</CardActions>
+						<CardMenu style={{ color: '#fff' }}>
+							<IconButton name="share" />
+						</CardMenu>
+					</Card>
 				</div>
 			);
 		} else if (this.state.activeTab === 1) {
@@ -36,7 +87,12 @@ class Projects extends Component {
 					<Tab>Python</Tab>
 					<Tab>MongoDB</Tab>
 				</Tabs>
-				<section className="projects-grid">{this.toggleCategories()}</section>
+
+				<Grid>
+					<Cell col={12}>
+						<div className="content">{this.toggleCategories()}</div>
+					</Cell>
+				</Grid>
 			</div>
 		);
 	}
