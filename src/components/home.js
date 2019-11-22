459b1cd6bef90630fe086fd8bab6abd9711202b3
beverlyosoria <bevosoria@gmail.com>
2019-11-21 23:15:43 -0600

    adds social links icons, styling

--- src/components/home.js
+++ src/components/home.js
@@ -11,6 +11,18 @@ class Home extends Component {
 						<img src={avatar} alt="avatar" className="avatar-img" />
 						<div className="banner-text">
 							<h1>Full Stack Web Developer</h1>
+							<hr />
+							<p>Skills I am good at here.</p>
+							<div className="social-links">
+								{/* Linked In */}
+								<a href="http://google.com" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-linkedin-square" aria-hidden="true" />
+								</a>
+								{/* Github */}
+								<a href="http://google.com" rel="noopener noreferrer" target="_blank">
+									<i className="fa fa-github-square" aria-hidden="true" />
+								</a>
+							</div>
 						</div>
 					</Cell>
 				</Grid>
