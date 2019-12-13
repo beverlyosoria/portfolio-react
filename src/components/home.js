7534ac4e1eb9aed15c9cbb4ee7cc798984e5391a
beverlyosoria <bevosoria@gmail.com>
2019-12-12 18:10:16 -0700

    removes tabs from projects

--- src/components/home.js
+++ src/components/home.js
@@ -6,11 +6,9 @@ import About from './about';
 import Projects from './projects';
 import Contact from './contact';
 
-
-
 // 	render() {
 // 		return (
-// 			<div className="home-grid">	
+// 			<div className="home-grid">
 // 				<div className="banner-text">
 // 					<img src={avatar} alt="avatar" className="avatar-img" />
 // 					<h1>Beverly Osoria</h1>
@@ -18,7 +16,7 @@ import Contact from './contact';
 // 						<p>Full Stack Engineer. Skills I am good at here.</p>
 // 						<div className="d-flex justify-content-center">
 // {/* Navigation */}
- 
+
 //  <ButtonGroup toggle className="mt-3">
 //     <ToggleButton className="nav-btn" type="radio" name="radio" defaultChecked value="1">
 //       <Link style={{ textDecoration: 'none'}}  to="/about">About</Link>
@@ -30,9 +28,9 @@ import Contact from './contact';
 //       <Link style={{ textDecoration: 'none'}} to="/contact">Contact</Link>
 //     </ToggleButton>
 //   </ButtonGroup>
-// 						</div>			
+// 						</div>
 // 				</div>
-				
+
 // 			</div>
 
 // 		);
@@ -42,7 +40,6 @@ import Contact from './contact';
 // export default Home;
 
 class Home extends Component {
-
 	constructor(props) {
 		super(props);
 		this.state = { activeTab: 0 };
@@ -52,16 +49,15 @@ class Home extends Component {
 			return (
 				<div className="home-grid">
 					{/* Project 1 */}
-					
-				<div className="banner-text">
- 					<img src={avatar} alt="avatar" className="avatar-img" />
- 					<h1>Beverly Osoria</h1>
- 						<hr />
- 						<p>Full Stack Engineer. Skills I am good at here.</p>
- 						<div className="d-flex justify-content-center"></div>
+
+					<div className="banner-text">
+						<img src={avatar} alt="avatar" className="avatar-img" />
+						<h1>Beverly Osoria</h1>
+						<hr />
+						<p>Full Stack Engineer. Skills I am good at here.</p>
+						<div className="d-flex justify-content-center" />
+					</div>
 				</div>
-				</div>
-			
 			);
 		} else if (this.state.activeTab === 1) {
 			return (
@@ -89,24 +85,22 @@ class Home extends Component {
 			<div className="home-grid">
 				{/* <ButtonGroup toggle className="mt-3"> */}
 				<Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-				{/* <ToggleButton className="nav-btn" type="radio" name="radio" defaultChecked value="1"> */}
+					{/* <ToggleButton className="nav-btn" type="radio" name="radio" defaultChecked value="1"> */}
 					<Tab className="nav-btn"> </Tab>
-						{/* </ToggleButton> */}
-						{/* <ToggleButton className="nav-btn" type="radio" name="radio" value="2"> */}
+					{/* </ToggleButton> */}
+					{/* <ToggleButton className="nav-btn" type="radio" name="radio" value="2"> */}
 					<Tab className="nav-btn">About</Tab>
-						{/* </ToggleButton> */}
-						 {/* <ToggleButton className="nav-btn" type="radio" name="radio" value="3">  */}
+					{/* </ToggleButton> */}
+					{/* <ToggleButton className="nav-btn" type="radio" name="radio" value="3">  */}
 					<Tab className="nav-btn">Projects</Tab>
 					<Tab className="nav-btn">Contact</Tab>
 				</Tabs>
-				
-			
+
 				<Grid>
 					<Cell col={12}>
 						<div className="content">{this.toggleCategories()}</div>
 					</Cell>
 				</Grid>
-				
 			</div>
 		);
 	}
