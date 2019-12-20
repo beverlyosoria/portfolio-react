74bc651a4ee3d3099fcf59cf3283a404c7f1f5a3
beverlyosoria <bevosoria@gmail.com>
2019-12-19 22:54:30 -0700

    adds projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,16 +15,18 @@ class Projects extends Component {
 								'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover'
 						}}
 					>
-						React Project 1
+						Guess That Movie
 					</CardTitle>
 					<CardText>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates consequuntur quo odio quia
-						reprehenderit distinctio qui et officiis illum facilis iusto molestiae dignissimos, commodi ea
-						rerum explicabo ut magni quos?
+						<p>
+							utilizing JavaScript to guess movie titles based on images. A game in which the player is
+							prompted with three images that are symbolic of a movie. The user must guess the movie title
+							to move on to the next set of images. The player wins when all movie titles are guessed
+							correctly.
+						</p>
 					</CardText>
 					<CardActions border>
-						<Button colored>GitHub</Button>
-						<Button colored>GitHub</Button>
+						<Button colored>Guess That Movie</Button>
 						<Button colored>GitHub</Button>
 					</CardActions>
 					<CardMenu style={{ color: '#fff' }}>
@@ -41,16 +43,48 @@ class Projects extends Component {
 								'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover'
 						}}
 					>
-						React Project 2
+						Search It
 					</CardTitle>
 					<CardText>
-						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates consequuntur quo odio quia
-						reprehenderit distinctio qui et officiis illum facilis iusto molestiae dignissimos, commodi ea
-						rerum explicabo ut magni quos?
+						<p>
+							add, create and update posts and reviews of local businesses A place where you can search
+							for deaf-friendly businesses in your area or share your consumer experience and insights as
+							deaf, deafblind, and hard of hearing individuals, friends/family of the deaf and those with
+							a special interest in creating a deaf-friendly world. The aim is to promote
+							deaf-friendliness in your community via awareness and feedback.
+						</p>
 					</CardText>
 					<CardActions border>
+						<Button colored>Search It</Button>
 						<Button colored>GitHub</Button>
-						<Button colored>GitHub</Button>
+					</CardActions>
+					<CardMenu style={{ color: '#fff' }}>
+						<IconButton name="share" />
+					</CardMenu>
+				</Card>
+				{/* Project 3 */}
+				<Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
+					<CardTitle
+						style={{
+							color: '#fff',
+							height: '176px',
+							background:
+								'url(https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png) center / cover'
+						}}
+					>
+						Happiest Hour
+					</CardTitle>
+					<CardText>
+						<p>
+							create group events and invite friends UX Designer, Front-end Developer · Happiest Hour
+							Happiest Hour simplifies organizing a happy hour with your coworkers, planning a weekend
+							with your friends, or coordinating a soccer practice with your teammates all in one place.
+							Through the app, the user can suggest a place, set a time and location and share privately
+							with a group of choice.
+						</p>
+					</CardText>
+					<CardActions border>
+						<Button colored>Happiest Hour</Button>
 						<Button colored>GitHub</Button>
 					</CardActions>
 					<CardMenu style={{ color: '#fff' }}>
