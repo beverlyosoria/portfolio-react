b61fffd8f8ecb56f3cc4cbdf0ed4245246099c93
beverlyosoria <bevosoria@gmail.com>
2020-01-05 23:24:33 -0600

    deletes unused code in home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,42 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import { Tabs, Tab, Grid, Cell } from 'react-mdl';
 import About from './about';
 import Projects from './projects';
 import Contact from './contact';
-
-// 	render() {
-// 		return (
-// 			<div className="home-grid">
-// 				<div className="banner-text">
-// 					<img src={avatar} alt="avatar" className="avatar-img" />
-// 					<h1>Beverly Osoria</h1>
-// 						<hr />
-// 						<p>Full Stack Engineer. Skills I am good at here.</p>
-// 						<div className="d-flex justify-content-center">
-// {/* Navigation */}
-
-//  <ButtonGroup toggle className="mt-3">
-//     <ToggleButton className="nav-btn" type="radio" name="radio" defaultChecked value="1">
-//       <Link style={{ textDecoration: 'none'}}  to="/about">About</Link>
-//     </ToggleButton>
-//     <ToggleButton className="nav-btn" type="radio" name="radio" value="2">
-//       <Link style={{ textDecoration: 'none'}} to="/projects">Projects</Link>
-//     </ToggleButton>
-//     <ToggleButton className="nav-btn" type="radio" name="radio" value="3">
-//       <Link style={{ textDecoration: 'none'}} to="/contact">Contact</Link>
-//     </ToggleButton>
-//   </ButtonGroup>
-// 						</div>
-// 				</div>
-
-// 			</div>
-
-// 		);
-// 	}
-// }
-
-// export default Home;
 
 class Home extends Component {
 	constructor(props) {
@@ -94,19 +60,10 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="home-grid">
-				{/* <ButtonGroup toggle className="mt-3"> */}
 				<Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-					{/* <ToggleButton className="nav-btn" type="radio" name="radio" defaultChecked value="1"> */}
-
 					<Tab id="nav-tab">HOME </Tab>
 
-					{/* </ToggleButton> */}
-					{/* <ToggleButton className="nav-btn" type="radio" name="radio" value="2"> */}
-
 					<Tab id="nav-tab">ABOUT</Tab>
-
-					{/* </ToggleButton> */}
-					{/* <ToggleButton className="nav-btn" type="radio" name="radio" value="3">  */}
 
 					<Tab id="nav-tab">PROJECTS</Tab>
 
