e16424e101caa53faa1bd8d099df2b1fb04dc876
beverlyosoria <bevosoria@gmail.com>
2020-01-06 18:53:30 -0600

    replaces contact js with stateful form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,37 +1,52 @@
 import React, { Component } from 'react';
 
-class Contact extends Component {
-	render() {
-		return (
-			<div className="banner-text">
-				<form name="contact" method="post">
-					<input type="hidden" name="contact" value="contact" />
+const encode = (data) => {
+	return Object.keys(data).map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
+};
 
-					<p>
-						<label>
-							Your Name: <input type="text" name="name" />
-						</label>
-					</p>
-					<p>
-						<label>
-							Your Email: <input type="email" name="email" />
-						</label>
-					</p>
-					<p>
-						<label>
-							Subject: <input type="subject" name="subject" />
-						</label>
-					</p>
-					<p>
-						<label>
-							Message: <textarea name="message" />
-						</label>
-					</p>
-					<p>
-						<button type="submit">Send</button>
-					</p>
-				</form>
-			</div>
+class Contact extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { name: '', email: '', message: '' };
+	}
+
+	handleSubmit = (e) => {
+		fetch('/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: encode({ 'form-name': 'contact', ...this.state })
+		})
+			.then(() => alert('Success!'))
+			.catch((error) => alert(error));
+
+		e.preventDefault();
+	};
+
+	handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+	render() {
+		const { name, email, message } = this.state;
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<p>
+					<label>
+						Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+					</label>
+				</p>
+				<p>
+					<label>
+						Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+					</label>
+				</p>
+				<p>
+					<label>
+						Message: <textarea name="message" value={message} onChange={this.handleChange} />
+					</label>
+				</p>
+				<p>
+					<button type="submit">Send</button>
+				</p>
+			</form>
 		);
 	}
 }
